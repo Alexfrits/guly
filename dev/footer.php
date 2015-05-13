ac3417414fabd89cffc604430991e5072298dcff
Alex Frits <frits.alex@gmail.com>
2015-05-13 09:37:47 +0200

    splitted to header-footer-page.php template

---
+++ dev/footer.php
@@ -0,0 +1,19 @@
+<!--  FOOTER
+==================================================================-->
+<footer>
+  
+</footer>
+<!-- end FOOTER -->
+
+<!--  SCRIPTS
+==================================================================-->
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+  <script>
+    window.jQuery || document.write('<script src="scripts/vendor/jquery-2.1.3.min.js"><\/script>')
+  </script>
+  <script src="scripts/vendor/foundation.min.js"></script>
+  <script src="scripts/main.min.js"></script>
+<!-- end SCRIPTS -->
+
+</body>
+</html>
---
+++ dev/index.php
@@ -0,0 +1,39 @@
+<?php
+  include 'header.php';
+?>
+    <!--  MAIN
+    ==================================================================-->
+    <main class="main-section main-wrapper">
+
+      <div class="mega-number__wrapper row">
+        <div class="mega-number small-12 small-centered text-center columns">
+          <strong>1,7</strong><span class="suffix">l</span>
+        </div>
+      </div>
+
+      <div class="tip__wrapper row">
+        <div class="small-12 small-centered text-center columns">
+          <q>
+            Ceci est un petit conseil qui apparaît quand vous avez tout bu.
+          </q>
+        </div>
+      </div>
+
+      <div class="add-button__wrapper row">
+        <div class="small-12 small-centered text-center columns">
+          <a class="button round" href="#">
+            <i class="fa fa-coffee"></i>
+          </a>
+        </div>
+      </div>
+
+    </main>
+    <!-- end MAIN -->
+
+  <a class="exit-off-canvas"></a>
+  </div>
+</div>
+
+<?php
+  include 'footer.php';
+?>

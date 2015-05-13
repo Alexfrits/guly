1f9525d19ac5ee4fcc9a69028833304be162d09f
Alex Frits <frits.alex@gmail.com>
2015-05-13 12:04:21 +0200

    layouted stats page

--- dev/statistiques.php
+++ dev/statistiques.php
@@ -2,28 +2,52 @@
     <!--  MAIN
     ==================================================================-->
     <main class="main-section main-wrapper">
-
-      <div class="mega-number__wrapper row">
-        <div class="mega-number small-12 small-centered text-center columns">
-          <strong>1,7</strong><span class="suffix">l</span>
-        </div>
-      </div>
-
-      <div class="tip__wrapper row">
+      <div class="row">
         <div class="small-12 small-centered text-center columns">
-          <q>
-            Ceci est un petit conseil qui apparaît quand vous avez tout bu.
-          </q>
+          <h1>Statistiques</h1>
         </div>
       </div>
 
-      <div class="add-button__wrapper row">
+      <div class="graphe__wrapper row">
+
+        <div class="graphe__scale--v small-2 columns">
+          <ul>
+            <li>0
+            <li>1
+            <li>2
+          </ul>
+        </div>
+        <div class="small-10 columns"> 
+          <div class="graphe__img__frame">
+            <div class="graphe__img">
+              <img src="img/guly-graphe.svg" alt="">
+            </div>
+          </div>       
+        </div>
+
+      </div>
+
+      <div class="row month-display">
         <div class="small-12 small-centered text-center columns">
-          <a class="button round" href="#">
-            <i class="fa fa-coffee"></i>
-          </a>
+          <ul>
+            <li><h3>MAI 2015</h3>
+          </ul>
         </div>
       </div>
+
+      <div class="row success-display__title">
+        <div class="small-12 small-centered text-center columns">
+          <h3>Derniers trophées</h3>
+        </div>
+      </div>
+      <ul class="row success-display__trophies">
+        <li class="small-6 text-center columns">
+          <i class="fa fa-trophy"></i>
+          <h4>Success Name</h4>
+        <li class="small-6 text-center columns">
+          <i class="fa fa-trophy"></i>
+          <h4>Success Name</h4>
+      </ul>
 
     </main>
     <!-- end MAIN -->
